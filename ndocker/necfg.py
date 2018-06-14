254b02590b4b8eb4400c737f237e144b119fc288
Haiyang Zhao <haiyang.zhao@nokia.com>
2018-06-14 17:48:11 +0800

    mod

--- ndocker/necfg.py
+++ ndocker/necfg.py
@@ -48,7 +48,8 @@ class ServicesCfg(object):
             return None
         
         container = self.services.get(container_name)
-        container['networks'] = ((i, self.networks[i]) for i in container['networks'])
+        networks = [(i, self.networks[i]) for i in container['networks']]
+        container['networks'] = networks
         return ContainerCfg(container_name, **container)
 
     def _verify_data(self):
