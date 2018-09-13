dc5741922a90038052d781515d1c53a3ef0d0f07
Haiyang Zhao <haiyang.zhao@nokia.com>
2018-09-13 16:15:33 +0800

    add version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class Install(install):
 
 
 setup(name='ndocker',
-      version='1.0.4',
+      version='1.0.5',
       description='docker network configration',
       long_description=long_description,
       url='http://github.com/codlin/ndocker',
